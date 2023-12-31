2107e615c179c3b3fb35066f47375b5cae526e9b
Yoweli Kachala <yowelikachala@gmail.com>
2023-12-31 17:24:17 +0200

    Refactored code and added wizard to be used for registration / settings

--- app/Http/Controllers/SettingController.php
+++ app/Http/Controllers/SettingController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Schedule;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 /**
  * Class SettingController
@@ -15,6 +17,26 @@ class SettingController extends Controller
      */
     public function index(): view
     {
-        return view('admin.setting.index');
+        $data = [
+            "Morning" => [
+                '5:00:00' => "wake up time",
+                '5:00:01' => "Exercise",
+                '6:00:00' => "Shower",
+                '7:00:00' => "Shower",
+                '7:30:00' => "Work",
+            ],
+            "Afternoon" => [
+                "12:00:00" => 'Lunch time',
+                "13:00:00" => 'Work',
+                "17:00:00" => 'Relax',
+            ],
+            "Evening" => [
+                '18:00:00' => "Relaxing time",
+                '19:00:00' => "Wind down",
+                '21:00:00' => "Sleep time",
+            ],
+        ];
+
+        return view('admin.setting.index', compact('data'));
     }
 }
--- resources/views/admin/setting/index.blade.php
+++ resources/views/admin/setting/index.blade.php
@@ -4,59 +4,93 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-12">
-                <div class="container p-0">
-
-                    <div class="row pt-3 pb-3">
-                        <div class="col-md-6">
-
-                            <div class="card">
-                                <div class="card-body">
-                                    <h5 class="card-title">Work times settings</h5>
-                                    <p class="card-text">Work Settings</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-md-6">
-                            <div class="card">
-                                <div class="card-body">
-                                    <h5 class="card-title">Eating times settings</h5>
-                                    <p class="card-text">Eating settings.</p>
-                                </div>
-                            </div>
-                        </div>
+                <div class="card">
+                    <div class="card-header">
+                        <h5>Settings</h5>
                     </div>
 
-                    <div class="row pt-3 pb-3">
-                        <div class="col-md-6">
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
 
-                            <div class="card">
-                                <div class="card-body">
-                                    <h5 class="card-title">Exercise times settings</h5>
-                                    <p class="card-text">Exercise Settings</p>
-                                </div>
+                        @if($errors->any())
+                            <div class="alert alert-danger">
+                                <ul>
+                                    @foreach($errors->all() as $error)
+                                        <li>{{ $error }}</li>
+                                    @endforeach
+                                </ul>
                             </div>
-                        </div>
-                        <div class="col-md-6">
-                            <div class="card">
-                                <div class="card-body">
-                                    <h5 class="card-title">Relaxation times settings</h5>
-                                    <p class="card-text">Relaxation settings.</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                        @endif
 
-                    <div class="row pt-3 pb-3">
-                        <div class="col-md-12">
-                            <div class="card">
-                                <div class="card-body">
-                                    Schedule
+                            <form id="wizardForm" action="" method="post">
+                                @csrf
+
+                                <div class="tab" id="resting">
+                                    <h5>Resting</h5>
+                                    <p>How many hours do you want to sleep</p>
+                                    <input type="text" name="sleep-hours" class="form-control">
+                                    <p>What time do you want to sleep</p>
+                                    <input type="text" name="sleep-hours" class="form-control">
+                                    <button type="button" class="btn btn-primary mt-3" onclick="nextStep('resting', 'exercise')">Next</button>
                                 </div>
-                            </div>
-                        </div>
+
+                                <div class="tab" id="exercise">
+                                    <h5>Exercises</h5>
+                                    <p>How many times can you exercise a day</p>
+                                    <input type="text" name="exercise_input" class="form-control">
+                                    <p>When can you exercise</p>
+                                    <input type="text" name="exercise_input" class="form-control">
+                                    <button type="button" class="btn btn-secondary mr-2 mt-3" onclick="prevStep('exercise', 'resting')">Previous</button>
+                                    <button type="button" class="btn btn-primary mt-3" onclick="nextStep('exercise', 'meals')">Next</button>
+                                </div>
+
+                                <div class="tab" id="meals">
+                                    <h5>Meals</h5>
+                                    <p>What times are you going to eat</p>
+                                    <input type="text" name="meals_input" class="form-control">
+                                    <button type="button" class="btn btn-secondary mr-2 mt-3" onclick="prevStep('meals', 'exercise')">Previous</button>
+                                    <button type="button" class="btn btn-primary mt-3" onclick="nextStep('meals', 'work')">Next</button>
+                                </div>
+
+                                <div class="tab" id="work">
+                                    <h5>Work</h5>
+                                    <p>What times are you going to eat</p>
+                                    <input type="text" name="meals_input" class="form-control">
+                                    <button type="button" class="btn btn-secondary mr-2 mt-3" onclick="prevStep('work', 'meals')">Previous</button>
+                                    <button type="submit" class="btn btn-success mt-3">Submit</button>
+                                </div>
+
+                            </form>
+
                     </div>
                 </div>
             </div>
         </div>
     </div>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+    <script>
+        function showTab(tabId) {
+            $('.tab').hide();
+            $('#' + tabId).show();
+        }
+
+        function nextStep(currentTab, nextTab) {
+            // Add validation logic if needed before proceeding to the next step
+
+            showTab(nextTab);
+        }
+
+        function prevStep(currentTab, prevTab) {
+            showTab(prevTab);
+        }
+
+        $(document).ready(function () {
+            showTab('resting');
+        });
+    </script>
 @endsection
